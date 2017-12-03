008e6909f61e624e2a5143eeeca1e2cfe7f74911
Trent Vasquez <vasquezt@oregonstate.edu>
2017-12-03 15:02:05 -0800

    Here is the work I have, for info text me

--- public/photoCardTemplate.js
+++ public/photoCardTemplate.js
@@ -3,10 +3,10 @@
 templates['photoCard'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<section class=\"photo-card\">\n  <div class=\"img-container\">\n    "
+  return "<section class=\"photo-card\">\r\n  <div class=\"img-container\">\r\n    "
     + alias4(((helper = (helper = helpers.exercise || (depth0 != null ? depth0.exercise : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"exercise","hash":{},"data":data}) : helper)))
-    + "\n  </div>\n  <div class=\"caption\">\n    "
+    + "\r\n  </div>\r\n  <div class=\"caption\">\r\n    "
     + alias4(((helper = (helper = helpers.weight || (depth0 != null ? depth0.weight : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"weight","hash":{},"data":data}) : helper)))
-    + "\n  </div>\n</section>\n";
+    + "\r\n  </div>\r\n</section>\r\n";
 },"useData":true});
 })();
--- server.js
+++ server.js
@@ -3,10 +3,18 @@ var fs = require('fs');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var peopleData = require('./peopleData');
 var app = express();
 var port = process.env.PORT || 2000;
+
+var connection = mysql.createConnection({
+	host: 'classmysql.engr.oregonstate.edu',
+	user: 'cs340_vasquezt',
+	password: '5132',
+	database: 'cs340_vasquezt'
+});
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -24,6 +32,7 @@ app.get('/people', function (req, res, next) {
 
 });
 
+
 app.get('/people/:person', function (req, res, next) {
   console.log("== url params for request:", req.params);
   var person = req.params.person;
@@ -37,6 +46,21 @@ app.get('/people/:person', function (req, res, next) {
     res.render('photosPage', templateArgs);
   } else {
     next();
+  }
+});
+
+app.post('/signup', function(req, res, next){
+  console.log("about to attempt to add a new account: ", req.params);
+  var split = document.location.href.replace('?', '').replace('&', '=').split('=')
+
+  var user = split[1];
+  var pass = split[3];
+  var email = split[5];
+  var salt = "123";
+  if(connection){
+    var query = connection.query('INSERT INTO Users (username, hashed_pass, salt, email) VALUES (${user}, ${pass}, ${salt}, ${email}', post, function(error, results, fields){
+      if(error) throw error;
+    });
   }
 });
 
@@ -81,3 +105,5 @@ app.get('*', function (req, res) {
 app.listen(port, function () {
   console.log("== Server is running on port number", port);
 });
+
+
